d5209b01b1ae6ec694c626fabf06a0335cf247ff
davidcoutts23 <davidjamescoutts@gmail.com>
2022-06-15 16:48:13 +1000

    Add place command

---
+++ lib/commands/place_command.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require './lib/robot'
+require './lib/position'
+require './lib/orientation'
+require './lib/commands/place_command_input_processor'
+
+class PlaceCommand
+  class PlaceCommandInvalidError < StandardError; end
+
+  def initialize(tabletop)
+    @tabletop = tabletop
+  end
+
+  def perform(input)
+    process_input_for(input)
+    validate_cardinal_direction
+    validate_position
+    place_robot
+  rescue StandardError => e
+    PlaceCommandInputProcessor::InputValidationError
+    Orientation::NonExistentCardinalDirectionError
+    Tabletop::NonExistentTabletopPositionError
+    puts e.message
+  end
+
+  attr_reader :robot
+
+  private
+
+  attr_reader :tabletop, :place_command_arguments
+
+  def process_input_for(input)
+    @place_command_arguments = PlaceCommandInputProcessor.new.perform(input)
+  end
+
+  def validate_cardinal_direction
+    Orientation.validate_cardinal_direction(place_command_arguments.cardinal_direction)
+  end
+
+  def validate_position
+    tabletop.validate_position(Position.new(
+                                 x_coordinate: place_command_arguments.x_coordinate,
+                                 y_coordinate: place_command_arguments.y_coordinate
+                               ))
+  end
+
+  def place_robot
+    puts 'robot placed'
+    @robot = Robot.new(
+      position: Position.new(x_coordinate: place_command_arguments.x_coordinate,
+                             y_coordinate: place_command_arguments.y_coordinate),
+      orientation: Orientation.new(cardinal_direction: place_command_arguments.cardinal_direction)
+    )
+  end
+end
---
+++ spec/commands/place_command_spec.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+RSpec.describe PlaceCommand do
+  subject(:place_command) { PlaceCommand.new(tabletop).perform(input) }
+
+  let(:tabletop) { double }
+
+  let(:input) { 'PLACE 10,20,NORTH' }
+  let(:x_coordinate) { 1 }
+  let(:y_coordinate) { 2 }
+  let(:cardinal_direction) { 'NORTH' }
+
+  let(:place_command_arguments) do
+    instance_double(
+      PlaceCommandArguments,
+      x_coordinate:,
+      y_coordinate:,
+      cardinal_direction:
+    )
+  end
+
+  let(:position) do
+    instance_double(
+      Position,
+      x_coordinate: place_command_arguments.x_coordinate,
+      y_coordinate: place_command_arguments.y_coordinate
+    )
+  end
+
+  let(:orientation) do
+    instance_double(
+      Orientation,
+      cardinal_direction: place_command_arguments.cardinal_direction
+    )
+  end
+
+  let(:place_command_input_processor) { double }
+
+  before(:each) do
+    allow(PlaceCommandInputProcessor).to receive(:new).and_return(place_command_input_processor)
+    allow(place_command_input_processor).to receive(:perform).with(input).and_return(place_command_arguments)
+
+    allow(Orientation).to receive(:validate_cardinal_direction).with(cardinal_direction).and_return(nil)
+    allow(tabletop).to receive(:validate_position).with(position).and_return(nil)
+
+    allow(Position).to receive(:new).with(x_coordinate:, y_coordinate:).and_return(position)
+    allow(Orientation).to receive(:new).with(cardinal_direction:).and_return(orientation)
+  end
+
+  describe '.perform' do
+    it 'places a robot at the specified position and orientation' do
+      expect(Robot).to receive(:new).with(orientation:, position:)
+      place_command
+    end
+
+    context 'the input processor raises an exception' do
+      before(:each) do
+        allow(place_command_input_processor).to receive(:perform) do
+          raise PlaceCommandInputProcessor::InputValidationError
+        end
+      end
+      it 'does not place a robot' do
+        expect(Robot).not_to receive(:new)
+        place_command
+      end
+    end
+
+    context 'cardinal direction validation raises an error' do
+      before(:each) do
+        allow(Orientation).to receive(:validate_cardinal_direction) do
+          raise Orientation::NonExistentCardinalDirectionError
+        end
+      end
+      it 'does not place a robot' do
+        expect(Robot).not_to receive(:new)
+        place_command
+      end
+    end
+
+    context 'position validation on tabletop raises an error' do
+      before(:each) do
+        allow(tabletop).to receive(:validate_position) do
+          raise Tabletop::NonExistentTabletopPositionError
+        end
+      end
+      it 'does not place a robot' do
+        expect(Robot).not_to receive(:new).with(position:, orientation:)
+        place_command
+      end
+    end
+  end
+end

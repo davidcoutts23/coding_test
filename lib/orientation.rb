b7a72af107ce65f90d2d0cac2f7a413692967b04
davidcoutts23 <davidjamescoutts@gmail.com>
2022-06-15 12:41:18 +1000

    Change orientation to raise an error is a non existed cardinal direction provided

--- lib/orientation.rb
+++ lib/orientation.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class Orientation
+  class NonExistentCardinalDirectionError < StandardError; end
+
   def initialize(cardinal_direction)
     @cardinal_direction = cardinal_direction
   end
 
-  def self.exist?(cardinal_direction)
-    cardinal_directions.include?(cardinal_direction)
+  def self.validate_cardinal_direction(cardinal_direction)
+    return if cardinal_directions.include?(cardinal_direction)
+
+    raise NonExistentCardinalDirectionError,
+          'Error: Cardinal direction does not exist.'
   end
 
   attr_reader :cardinal_direction
--- spec/orientation_spec.rb
+++ spec/orientation_spec.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
-require 'orientation'
+require 'spec_helper'
 
 RSpec.describe Orientation do
+  let(:validate_cardinal_direction) { described_class.validate_cardinal_direction(cardinal_direction) }
   let(:cardinal_direction) { 'NORTH' }
 
-  describe '#exist?' do
-    it 'returns true' do
-      expect(described_class.exist?(cardinal_direction)).to be(true)
+  describe '#validate_cardinal_direction' do
+    it 'returns with no exception' do
+      expect { validate_cardinal_direction }.to_not raise_error(
+        Orientation::NonExistentCardinalDirectionError,
+        'Error: Cardinal direction does not exist.'
+      )
     end
 
     context 'the orientation provided does not exist' do
       let(:cardinal_direction) { 'SOUTHEAST' }
-      it 'returns false' do
-        expect(described_class.exist?(cardinal_direction)).to be(false)
+      it 'raises an exception' do
+        expect { validate_cardinal_direction }.to raise_error(
+          Orientation::NonExistentCardinalDirectionError,
+          'Error: Cardinal direction does not exist.'
+        )
       end
     end
   end

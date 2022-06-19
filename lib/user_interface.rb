c23fcc69338d2149e8c18095a5d4acd7c5d6b75a
davidcoutts23 <davidjamescoutts@gmail.com>
2022-06-19 20:27:43 +1000

    upcase input

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -9,7 +9,7 @@ module UserInterface
     loop do
       print 'Input command: '
       input = gets
-      simulation_orchestrator.command_for(input.strip)
+      simulation_orchestrator.command_for(input.strip.upcase)
       puts simulation_orchestrator.result
     end
   end

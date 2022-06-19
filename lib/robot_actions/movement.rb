af59572621abe5b8dafae6e60bd1f41925d7209d
davidcoutts23 <davidjamescoutts@gmail.com>
2022-06-20 09:42:10 +1000

    clean up movement

--- lib/robot_actions/movement.rb
+++ lib/robot_actions/movement.rb
@@ -7,23 +7,19 @@ class Movement
     case orientation.cardinal_direction
     when 'NORTH'
       Position.new(
-        x_coordinate: position.x_coordinate,
-        y_coordinate: position.y_coordinate + 1
+        x_coordinate: position.x_coordinate, y_coordinate: position.y_coordinate + 1
       )
     when 'SOUTH'
       Position.new(
-        x_coordinate: position.x_coordinate,
-        y_coordinate: position.y_coordinate - 1
+        x_coordinate: position.x_coordinate, y_coordinate: position.y_coordinate - 1
       )
     when 'EAST'
       Position.new(
-        x_coordinate: position.x_coordinate + 1,
-        y_coordinate: position.y_coordinate
+        x_coordinate: position.x_coordinate + 1, y_coordinate: position.y_coordinate
       )
     when 'WEST'
       Position.new(
-        x_coordinate: position.x_coordinate - 1,
-        y_coordinate: position.y_coordinate
+        x_coordinate: position.x_coordinate - 1, y_coordinate: position.y_coordinate
       )
     end
   end
